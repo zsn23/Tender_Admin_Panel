997b5143b2804e2986fb6783bd1bd36b05b382c6
Muhammad Zeeshan <zeeshanijaz14@gmail.com>
2024-08-26 20:58:02 +0500

    Working on sidebar

--- adminPanel/src/layouts/components/sidebar/sidebar.js
+++ adminPanel/src/layouts/components/sidebar/sidebar.js
@@ -1,3 +1,170 @@
+// import React, { Component, Fragment } from "react";
+// import classnames from "classnames";
+// import "../../../assets/scss/components/sidebar/sidebar.scss";
+// import SideMenuContent from "./sidemenu/sidemenu";
+// import SidebarHeader from "./sidebarHeader/sidebarHeader";
+// import { FoldedContentConsumer } from "../../../utility/context/toggleContentContext";
+// import templateConfig from "../../../templateConfig";
+// import Customizer from "../../../components/customizer/customizer";
+// import _EventEmitter from "./../../../constants/emitter"
+
+
+// class Sidebar extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       collapsedSidebar: true,
+//       width: window.innerWidth,
+//       sidebarState: "close",
+
+//     };
+
+//     _EventEmitter.on("handleLeftSideBar", this.handleLeftSideBar)
+//   }
+
+
+//   updateWidth = () => {
+//     if (window.innerWidth <= 991) {
+//       this.setState({ collapsedSidebar: true });
+//     } else {
+//       this.setState({ collapsedSidebar: true });
+//     }
+//     this.setState((prevState) => ({
+//       width: window.innerWidth,
+//     }));
+//   };
+
+//   handleCollapsedSidebar = (collapsedSidebar) => {
+//     this.setState({ collapsedSidebar: collapsedSidebar });
+//   };
+
+//   componentDidMount() {
+//     if (window) {
+//       window.addEventListener("resize", this.updateWidth, false);
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     if (window) {
+//       window.removeEventListener("resize", this.updateWidth, false);
+//     }
+//   }
+//   handleMouseEnter = (e) => {
+//     this.setState((prevState) => ({
+//       collapsedSidebar: false,
+//     }));
+//   };
+
+//   handleMouseLeave = (e) => {
+//     this.setState((prevState) => ({
+//       collapsedSidebar: true,
+//     }));
+//   };
+
+//   handleLeftSideBar = () => {
+//     if (this.state.sidebarState == "open") {
+//       this.setState({ sidebarState: "close" })
+//     }
+//     else {
+//       this.setState({ sidebarState: "open" })
+//     }
+//     // this.setState((prevState) => ({
+//     //   collapsedSidebar: true,
+//     // }));
+//   }
+
+//   render() {
+//     return (
+//       <Fragment>
+//         <FoldedContentConsumer>
+//           {(context) => (
+//             <div
+//               data-active-color="white"
+//               data-background-color={
+//                 this.props.color === ""
+//                   ? templateConfig.sidebar.backgroundColor
+//                   : this.props.color
+//               }
+//               className={classnames(
+//                 "app-sidebar",
+//                 {
+//                   "": !this.state.collapsedSidebar,
+//                   collapsed: this.state.collapsedSidebar,
+//                 },
+//                 {
+//                   "hide-sidebar":
+//                     this.state.width < 991 &&
+//                     this.state.sidebarState === "close",
+//                   "": this.state.sidebarState === "open",
+//                 }
+//               )}
+//               onMouseEnter={context.foldedContent ? null : null}
+//               onMouseLeave={
+//                 context.foldedContent ? this.handleMouseLeave : null
+//               }
+//             >
+//               <SidebarHeader
+//                 toggleSidebarMenu={this.props.toggleSidebarMenu}
+//                 sidebarBgColor={this.props.color}
+//               />
+//               <div className="sidebar-content">
+//                 <SideMenuContent
+//                   collapsedSidebar={this.state.collapsedSidebar}
+//                   toggleSidebarMenu={this.props.toggleSidebarMenu}
+//                 />
+//               </div>
+
+//               {/* {this.props.img === '' ? ( */}
+//               {templateConfig.sidebar.backgroundImage ? (
+//                 this.props.imgurl === "" ? (
+//                   <div
+//                     className="sidebar-background"
+//                     style={{
+//                       backgroundImage:
+//                         "url('" +
+//                         templateConfig.sidebar.backgroundImageURL +
+//                         "')",
+//                     }}
+//                   ></div>
+//                 ) : (
+//                   <div
+//                     className="sidebar-background"
+//                     style={{
+//                       backgroundImage: "url('" + this.props.imgurl + "')",
+//                     }}
+//                   ></div>
+//                 )
+//               ) : this.props.imgurl === "" ? (
+//                 <div className="sidebar-background"></div>
+//               ) : (
+//                 <div
+//                   className="sidebar-background"
+//                   style={{
+//                     backgroundImage: "url('" + this.props.imgurl + "')",
+//                   }}
+//                 ></div>
+//               )}
+//             </div>
+//           )}
+//         </FoldedContentConsumer>
+
+//         <Customizer
+//           sidebarBgColor={this.props.sidebarBgColor}
+//           sidebarImageUrl={this.props.sidebarImageUrl}
+//           sidebarCollapsed={this.props.sidebarCollapsed}
+//           handleSidebarSize={this.props.handleSidebarSize}
+//           handleLayout={this.props.handleLayout}
+//           handleCollapsedSidebar={this.handleCollapsedSidebar.bind(this)}
+//           toggleThemeCustomizer={this.props.toggleThemeCustomizer}
+//           customizer={this.props.customizerState}
+//         />
+//       </Fragment>
+//     );
+//   }
+// }
+
+// export default Sidebar;
+
 import React, { Component, Fragment } from "react";
 import classnames from "classnames";
 import "../../../assets/scss/components/sidebar/sidebar.scss";
@@ -6,8 +173,7 @@ import SidebarHeader from "./sidebarHeader/sidebarHeader";
 import { FoldedContentConsumer } from "../../../utility/context/toggleContentContext";
 import templateConfig from "../../../templateConfig";
 import Customizer from "../../../components/customizer/customizer";
-import _EventEmitter from "./../../../constants/emitter"
-
+import _EventEmitter from "./../../../constants/emitter";
 
 class Sidebar extends Component {
   constructor(props) {
@@ -16,62 +182,63 @@ class Sidebar extends Component {
       collapsedSidebar: true,
       width: window.innerWidth,
       sidebarState: "close",
-
+      mouseEntered: false,
     };
 
-    _EventEmitter.on("handleLeftSideBar", this.handleLeftSideBar)
+    this.sidebarRef = React.createRef(); // Initialize a ref for the sidebar
+    _EventEmitter.on("handleLeftSideBar", this.handleLeftSideBar);
   }
 
-
   updateWidth = () => {
-    if (window.innerWidth <= 991) {
-      this.setState({ collapsedSidebar: true });
-    } else {
-      this.setState({ collapsedSidebar: true });
-    }
-    this.setState((prevState) => ({
+    this.setState({
       width: window.innerWidth,
-    }));
+      collapsedSidebar: window.innerWidth <= 991,
+    });
   };
 
   handleCollapsedSidebar = (collapsedSidebar) => {
-    this.setState({ collapsedSidebar: collapsedSidebar });
+    this.setState({ collapsedSidebar });
   };
 
   componentDidMount() {
     if (window) {
       window.addEventListener("resize", this.updateWidth, false);
+      window.addEventListener("click", this.handleClickOutside, false);
     }
   }
 
   componentWillUnmount() {
     if (window) {
       window.removeEventListener("resize", this.updateWidth, false);
+      window.removeEventListener("click", this.handleClickOutside, false);
     }
   }
-  handleMouseEnter = (e) => {
-    this.setState((prevState) => ({
-      collapsedSidebar: false,
-    }));
+
+  handleMouseEnter = () => {
+    this.setState({ mouseEntered: true, collapsedSidebar: false });
   };
 
-  handleMouseLeave = (e) => {
-    this.setState((prevState) => ({
-      collapsedSidebar: true,
-    }));
+  handleMouseLeave = () => {
+    if (!this.state.mouseEntered) {
+      this.setState({ collapsedSidebar: true });
+    }
+  };
+
+  handleIconClick = () => {
+    this.setState({ mouseEntered: true }); // Ensure sidebar stays expanded on icon click
+  };
+
+  handleClickOutside = (event) => {
+    if (this.sidebarRef.current && !this.sidebarRef.current.contains(event.target)) {
+      this.setState({ collapsedSidebar: true, mouseEntered: false });
+    }
   };
 
   handleLeftSideBar = () => {
-    if (this.state.sidebarState == "open") {
-      this.setState({ sidebarState: "close" })
-    }
-    else {
-      this.setState({ sidebarState: "open" })
-    }
-    // this.setState((prevState) => ({
-    //   collapsedSidebar: true,
-    // }));
-  }
+    this.setState((prevState) => ({
+      sidebarState: prevState.sidebarState === "open" ? "close" : "open",
+    }));
+  };
 
   render() {
     return (
@@ -79,6 +246,7 @@ class Sidebar extends Component {
         <FoldedContentConsumer>
           {(context) => (
             <div
+              ref={this.sidebarRef}
               data-active-color="white"
               data-background-color={
                 this.props.color === ""
@@ -88,8 +256,8 @@ class Sidebar extends Component {
               className={classnames(
                 "app-sidebar",
                 {
-                  "": !this.state.collapsedSidebar,
-                  collapsed: this.state.collapsedSidebar,
+                  "expanded": !this.state.collapsedSidebar,
+                  "collapsed": this.state.collapsedSidebar,
                 },
                 {
                   "hide-sidebar":
@@ -98,14 +266,13 @@ class Sidebar extends Component {
                   "": this.state.sidebarState === "open",
                 }
               )}
-              onMouseEnter={context.foldedContent ? null : null}
-              onMouseLeave={
-                context.foldedContent ? this.handleMouseLeave : null
-              }
+              onMouseEnter={this.handleMouseEnter}
+              onMouseLeave={this.handleMouseLeave}
             >
               <SidebarHeader
                 toggleSidebarMenu={this.props.toggleSidebarMenu}
                 sidebarBgColor={this.props.color}
+                onIconClick={this.handleIconClick} // Pass down the click handler
               />
               <div className="sidebar-content">
                 <SideMenuContent
@@ -114,33 +281,22 @@ class Sidebar extends Component {
                 />
               </div>
 
-              {/* {this.props.img === '' ? ( */}
               {templateConfig.sidebar.backgroundImage ? (
-                this.props.imgurl === "" ? (
-                  <div
-                    className="sidebar-background"
-                    style={{
-                      backgroundImage:
-                        "url('" +
-                        templateConfig.sidebar.backgroundImageURL +
-                        "')",
-                    }}
-                  ></div>
-                ) : (
-                  <div
-                    className="sidebar-background"
-                    style={{
-                      backgroundImage: "url('" + this.props.imgurl + "')",
-                    }}
-                  ></div>
-                )
+                <div
+                  className="sidebar-background"
+                  style={{
+                    backgroundImage: this.props.imgurl
+                      ? `url('${this.props.imgurl}')`
+                      : `url('${templateConfig.sidebar.backgroundImageURL}')`,
+                  }}
+                ></div>
               ) : this.props.imgurl === "" ? (
                 <div className="sidebar-background"></div>
               ) : (
                 <div
                   className="sidebar-background"
                   style={{
-                    backgroundImage: "url('" + this.props.imgurl + "')",
+                    backgroundImage: `url('${this.props.imgurl}')`,
                   }}
                 ></div>
               )}
@@ -154,7 +310,7 @@ class Sidebar extends Component {
           sidebarCollapsed={this.props.sidebarCollapsed}
           handleSidebarSize={this.props.handleSidebarSize}
           handleLayout={this.props.handleLayout}
-          handleCollapsedSidebar={this.handleCollapsedSidebar.bind(this)}
+          handleCollapsedSidebar={this.handleCollapsedSidebar}
           toggleThemeCustomizer={this.props.toggleThemeCustomizer}
           customizer={this.props.customizerState}
         />
@@ -164,4 +320,3 @@ class Sidebar extends Component {
 }
 
 export default Sidebar;
-
